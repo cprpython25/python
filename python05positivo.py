0b6b4c2cfcc5dc45d875d23f336e1530a0c6250e
cprpython25 <cpellico79@gmail.com>
2025-02-07 20:53:39 +0100

    FINDEVIERNES

---
+++ python05positivo.py
@@ -0,0 +1,12 @@
+print("NUMERO POSITIVO O NEGATIVO")
+print("introduzca un numero")
+print("introduzca un numero")
+  if (numero > 0):
+    print("positivo")
+Elif (numero==)
+    print("cero")
+Else:
+    print("Negativo")
+print("Fin de programa")
+  
+
---
+++ python06elnumeromayor.py
@@ -0,0 +1,5 @@
+print("Numero mayor de 2 numeros")
+print("introduzca un numero")
+numero1 = int(input())
+print("introduzca numero 2")
+numero2 = int(input())
